1efd7a9f4970cda7c92709751800da2ca2a9c27b
Jose Ruben Daza Hernandez <rudahee@gmail.com>
2019-07-26 13:19:49 +0200

    SimpleChat

--- Cliente.py
+++ Cliente.py
@@ -1,38 +1,49 @@
 import socket
 import threading
 
-
 class ClaseCliente():
+
 	def __init__(self):
+
+		# Configuramos el tipo de conexion y nos conectamos al servidor.
 		self.cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.cliente.connect(('localhost', 1337))
 
 		self.nick = input("Selecciona tu nombre de usuario: ")
 
+		# Ponemos un thread a recibir los mensajes.
 		mensajeRecibido = threading.Thread(target=self.RecibirMensajes)
 		mensajeRecibido.daemon = True
 		mensajeRecibido.start()
 
+		# Bucle que mantiene vivo el bucle y ademas nos permite enviar mensajes.
 		while True:
 			mensaje = input()
-			if mensaje != "salir":
-				self.EnviarMensajes(mensaje)
-			else:
+			try:
+				if mensaje != "salir":
+					self.EnviarMensajes(mensaje)
+				else:
+					self.cliente.close()
+			except:
 				self.cliente.close()
 
 
 	def RecibirMensajes(self):
-		while True:
+
+		while True: # Bucle que mantiene viva la recepcion de mensajes.
 			try:
 				mensaje = self.cliente.recv(2048)
-				print(mensaje.decode())
+				print(mensaje.decode()) # Por defecto el encode es "utf-8".
 			except:
 				pass
 
-	def EnviarMensajes(self, mensaje):
-		mensaje = self.nick + "- " + mensaje 
 
-		self.cliente.send(bytes(mensaje.encode()))
+	def EnviarMensajes(self, mensaje):
+
+		mensaje = self.nick + "- " + mensaje # Agregamos el nick al mensaje. 
+		self.cliente.send(bytes(mensaje.encode())) # Enviamos el mensaje codificado al servidor. Por defecto, en "utf-8".
+
+# --------------------------------------------------------------#
 
 
 start = ClaseCliente()
--- Servidor.py
+++ Servidor.py
@@ -1,26 +1,30 @@
 import socket
 import threading
 
-
 class ClaseServidor(): 
 
 	def __init__(self):
+
 		# Configuramos el tipo de conexion y nos ponemos a escuchar 
 		self.servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.servidor.bind(('localhost', 1337))
 		self.servidor.listen()
-		self.servidor.setblocking(False)
+		self.servidor.setblocking(False) # No bloqueamos la conexion (Genera una excepcion si no puede mandar o recibir datos).
 
+		# Ponemos un thread a aceptar las conexiones.
 		aceptarConex = threading.Thread(target=self.AceptarConexiones)
 		aceptarConex.daemon = True
 		aceptarConex.start()
 
+		# Ponemos un thread a leer y reenviar los mensajes entrantes.
 		manejoMensajes = threading.Thread(target=self.ManejarMensajesEntrantes)
 		manejoMensajes.daemon = True
 		manejoMensajes.start()
 
+
+		# Bucle que mantiene vivo el servidor.
 		try:
-			while True:
+			while True: 
 				Mensaje = input(" =>  ")
 				if Mensaje == "salir":
 					self.servidor.close()
@@ -28,35 +32,44 @@ class ClaseServidor():
 		except:
 			self.servidor.close()
 
+
 	def MandarMensajes(self, mensaje, emisor):
-		for receptor in self.listaConexiones:
-			try:
+		
+		for receptor in self.listaConexiones:	
+			try: 
+				# Si la persona que envia el mensaje no es el que lo recibe, envia el mensaje.
 				if emisor != receptor:
 					receptor.send(mensaje)
 			except:
+				# Entramos en la excepcion si no se puede enviar el mensaje a alguien, y lo quitamos de la lista de clientes.
 				self.listaConexiones.remove(cliente)
+
 
 	def AceptarConexiones(self):
 
-		self.listaConexiones = []
+		self.listaConexiones = [] # Lista para guardar las conexiones
 
-		while True:
+		while True: # Bucle que mantiene escuchando 
 			try:
-				clienteConexion, clienteIP = self.servidor.accept()
+				clienteConexion, clienteIP = self.servidor.accept() # Empezamos a aceptar conexiones 
 				clienteConexion.setblocking(False)
-				self.listaConexiones.append(clienteConexion)
+				self.listaConexiones.append(clienteConexion) # Agregamos el objeto cliente a nuestra lista.
 				print("se ha conectado el cliente: ", clienteIP[0])
 			except:
-				pass
+				pass # Esto es para el setblocking, si se puede aceptar, da una excepcion.
+
 
 	def ManejarMensajesEntrantes(self):
-		while True:
-			if len(self.listaConexiones) != 0:
-				for cliente in self.listaConexiones:
+
+		while True: # Bucle que mantiene vivo el manejo de mensajes.
+			if len(self.listaConexiones) != 0: 
+				for cliente in self.listaConexiones: 
 					try:
-						mensaje = cliente.recv(2048)
-						self.MandarMensajes(mensaje, cliente)
+						mensaje = cliente.recv(2048) # Recibimos un mensaje.
+						self.MandarMensajes(mensaje, cliente) # Lo mandamos a enviar.
 					except:
-						pass
+						pass # Esto es para el setblocking, si no recibe un mensaje, da una excepcion.
+
+# --------------------------------------------------------------#
 
 start = ClaseServidor()
